72eba5b8cb4b9d037152d6268d013ff7af969909
Seetha1001 <139128350+Seetha1001@users.noreply.github.com>
2023-07-20 13:18:13 +0530

    day02
    Program2

---
+++ src/day02/Program2.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+boolean x = 50>2;
+System.out.println(x);
+boolean y = 50<2;
+System.out.println(y);
+	}
+
+}
